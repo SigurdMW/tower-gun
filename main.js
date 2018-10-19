8c91e4af21f32471f9e24024ace31165ffbd0c6b
SigurdMW <sigurdmwahl@gmail.com>
2018-10-19 23:02:10 +0200

    babel, async/await and image loading and display in canvas

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
+// eslint-disable-next-line
+import "babel-polyfill"
+
+(async () => {
+    
 const canvas = document.getElementById("tower-gun")
 const startBtn = document.getElementById("start-game")
+
+const towerImageSrc = require("./img/tower.jpg")
+const goodGuyImageSrc = require("./img/good-guy.jpg")
+const badGuyImageSrc = require("./img/bad-guy.jpg")
+const dollarImageSrc = require("./img/dollar.jpg")
 
 document.addEventListener("keydown", (e) => {
     if (e.keyCode === 37) { // left
@@ -8,6 +18,32 @@ document.addEventListener("keydown", (e) => {
         if ((gunState.hPos + gunSize) < canvas.width) gunState.hPos += 10
     }
 })
+
+const NUM_COLUMNS = 10
+const COL_SIZE = canvas.width / NUM_COLUMNS
+
+const newImageLoader = (imgPath) => {
+    return new Promise((resolve, reject) => {
+        const image = new Image()
+        image.onload = () => {
+            resolve(image)
+        }
+        image.onerror = (e) => {
+            reject(e)
+        }
+        image.src = imgPath
+    })
+}
+
+// include needed resources here
+const [towerImage, goodGuyImage, badGuyImage, dollarImage] = await Promise.all([
+    newImageLoader(towerImageSrc),
+    newImageLoader(goodGuyImageSrc),
+    newImageLoader(badGuyImageSrc),
+    newImageLoader(dollarImageSrc)
+])
+
+console.log({goodGuyImage, badGuyImage, dollarImage})
 
 const bullets = []
 
@@ -31,15 +67,24 @@ const drawBackground = () => {
 }
 
 const drawGun = () => {
-    context.fillStyle = "#000"
-    context.fillRect(gunState.hPos, canvas.height - gunSize, gunSize, gunSize)
+    context.drawImage(towerImage,gunState.hPos, canvas.height - gunSize, gunSize, gunSize)
 }
 
+const drawBadGuys = () => {
+    (new Array(10)).fill(0).map((n, i) => {
+        context.drawImage(badGuyImage, i * COL_SIZE, 0, COL_SIZE, COL_SIZE)
+    })
+    
+}
+   
 const update = (time = 0) => {
     drawBackground()
     drawGun()
+    drawBadGuys()
     requestAnimationFrame(update)
 }
 
 drawGun()
 update()
+
+})() // end async
